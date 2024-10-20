8caa3debf1464c18fcfcda7a9c6a2f984715d2d1
MalikIbrahim26 <malikibrahim1062@gmail.com>
2024-10-20 07:47:28 +0700

    test commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { default: daisyui } = require('daisyui');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
